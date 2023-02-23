dc641eb17cc8c7ea43d4c4e9cd1522438cfeb48a
Nikhil-Pradhan <nikhilpradhan1000@gmail.com>
2023-02-23 22:17:54 +0530

    parserdef.h

---
+++ parserdef.h
@@ -0,0 +1,71 @@
+#include "lexerDef.h"
+#include <stdlib.h>
+
+struct grammarchar {
+    char* val;
+    int type;
+};
+
+struct ListNode {
+    struct grammarchar val;
+    struct ListNode* next;
+};
+
+struct TreeNode {
+    int test;
+};
+
+struct StackNode {
+    struct TreeNode* val;
+    struct StackNode* next;
+};
+
+void insert (ListNode* head, struct grammarchar gc) {
+    ListNode* newnode = (struct ListNode *)malloc(sizeof(struct ListNode));
+    newnode -> val = gc;
+    newnode -> next = NULL;
+    if (head == NULL)
+        head = newnode;
+    else {
+        ListNode* curr = head;
+        while (curr -> next != NULL)
+            curr = curr -> next;
+        curr -> next = newnode;
+    }
+}
+
+void deletelast (struct ListNode* head) {
+    if (head != NULL) {
+        if (head -> next == NULL)
+            head = NULL;
+        struct ListNode* curr = head;
+        while (curr -> next -> next != NULL)
+            curr = curr -> next;
+        curr -> next = NULL;
+    }
+}
+
+int isEmpty (struct StackNode* top) {
+    return top == NULL;
+}
+
+void push (struct StackNode* top, struct TreeNode* newnode) {
+    struct StackNode* newstacknode = (struct StackNode*)malloc(sizeof(struct StackNode));
+    newstacknode -> val = newnode;
+    newstacknode -> next = top;
+    top = newstacknode;
+}
+
+struct TreeNode* peek (struct StackNode* top) {
+    if (isEmpty(top))
+        return NULL;
+    return top -> val;
+}
+
+struct TreeNode* pop (struct StackNode* top) {
+    if (isEmpty(top))
+        return NULL;
+    struct StackNode* curr = top;
+    top = top -> next;
+    return curr -> val;
+}
