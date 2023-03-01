0a4124957c6995d64ce4f3ac6deb92d21b4da741
Shreekar-P <84306985+Shreekar-P@users.noreply.github.com>
2023-03-01 23:08:13 +0530

    Update printTree.c

--- printTree.c
+++ printTree.c
@@ -3,10 +3,10 @@ void printTree(TreeNode* root) {
     if(root==NULL) return;
     TreeNode* curr = root->child;
     printTree(root->child);
-    if(root.t==TERMINAL && tokentype==NUM) printf("Terminal %s\n"); //need mapping functoin
-    else if(root.t==TERMINAL && tokentype==RNUM) {}
-    else if(root.t==TERMINAL) {}
-    else printf("Non-Terminal %s\n"); //need mapping function
+    if(root->t==TERMINAL && root->val.t.type==TK_NUM) printf("Terminal %s with token value %d\n", maptokentostring(root->val.t.type),root->val.t.val.integer);
+    else if(root->t==TERMINAL && root->val.t.type==TK_RNUM) {("Terminal %s with token value %lf\n", maptokentostring(root->val.t.type),root->val.t.val.decimal);}
+    else if(root->t==TERMINAL) {("Terminal %s with token value %d\n", maptokentostring(root->val.t.type),root->val.t.val.identifier);}
+    else printf("Non-Terminal %s\n",mapnttostring(root->val.nt));
     curr=curr->sibling;
     for(;curr!=NULL; curr=curr->sibling){
         printTree(curr);
