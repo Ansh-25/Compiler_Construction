9ff4ccdcc167a4c9f0267ef3c6898882115272d3
Toshit Jain <toshitjain@Toshits-MacBook-Air.local>
2023-03-01 18:05:23 +0530

    parse done

--- parsertesting.c
+++ parsertesting.c
@@ -4,26 +4,40 @@ int main() {
     loadgrammar("GrammarForParser");
     computefirstandfollow();
     createParseTable();
-    printf("FIRST :- \n");
-    for (int i = 0; i < NO_NONTERMS; i ++) {
-        printf("\nFirst set of %d: \n", i);
-        for (ListNode* curr = first[i]; curr != NULL; curr = curr -> next) {
-            printf("%s ",mapttokentostring(curr -> val.g.t));
-        }
-    }
-    printf("\n\n\nFOLLOW :- \n");
-    for (int i = 0; i < NO_NONTERMS; i ++) {
-        printf("\nFollow set of %d: \n", i);
-        for (ListNode* curr = follow[i]; curr != NULL; curr = curr -> next) {
-            printf("%s ",mapttokentostring(curr -> val.g.t));
-        }
-    }
+    // printf("FIRST :- \n");
+    // for (int i = 0; i < NO_NONTERMS; i ++) {
+    //     printf("\nFirst set of %d: \n", i);
+    //     for (ListNode* curr = first[i]; curr != NULL; curr = curr -> next) {
+    //         printf("%s ",mapttokentostring(curr -> val.g.t));
+    //     }
+    // }
+    // printf("\n\n\nFOLLOW :- \n");
+    // for (int i = 0; i < NO_NONTERMS; i ++) {
+    //     printf("\nFollow set of %d: \n", i);
+    //     for (ListNode* curr = follow[i]; curr != NULL; curr = curr -> next) {
+    //         printf("%s ",mapttokentostring(curr -> val.g.t));
+    //     }
+    // }
 
-    printf("\n");
+    // printf("\n");
     
-    for (int i = 0; i < NO_NONTERMS; i ++) {
-        for (int j = 0; j < NO_TERMS - 1; j ++) {
-            printf("%d ",parseTable[i][j]);
+    // for (int i = 0; i < NO_NONTERMS; i ++) {
+    //     for (int j = 0; j < NO_TERMS - 1; j ++) {
+    //         printf("%d ",parseTable[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    ListNode* curr;
+    for (int i = 0; i < NO_RULES; i ++) {
+        printf("RULE: %d\n",i);
+        curr = grammar[i];
+        while (curr != NULL) {
+            if (curr -> val.t == TERMINAL)
+                printf("%d ", curr -> val.g.t);
+            else
+                printf("%d ", curr -> val.g.nt);
+            curr = curr -> next;
         }
         printf("\n");
     }
