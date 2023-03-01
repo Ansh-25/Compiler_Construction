659094d3ace45fa351363d5a58db95363625a740
Nikhil-Pradhan <nikhilpradhan1000@gmail.com>
2023-03-01 12:47:52 +0530

    .

--- createTableTesting.c
+++ createTableTesting.c
@@ -8,7 +8,7 @@
 #define NO_RULES 141
 #define NO_TERMS 57
 #define NO_NONTERMS 72
-#define MAXTERMLEN 31
+#define MAXTERMLEN 50
 
 struct ListNode* grammar[NO_RULES];
 struct ListNode* first[NO_NONTERMS];
