f1e853dfa6dfa80a805b753281cd6d3dc16b447d
Toshit Jain <toshitjain@Toshits-MacBook-Air.local>
2023-04-04 16:31:40 +0530

    typechecker.c

--- typeChecker.c
+++ typeChecker.c
@@ -1,4 +1,14 @@
 #include "symbolTableDef.h"
 
 MainTableEntry** SymbolTable;
+ModuleTableEntry** curr; //set curr at every module node
 
+/* how to handle if module reuse before module definition */
+
+typechecker(ASTNode* astNode){
+    int c = astNode->label;
+    switch(c){
+        
+
+    }
+}
