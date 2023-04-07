1c66b3c81d5a9e86f0c4ba2e47a48fd217aac811
Nikhil-Pradhan <nikhilpradhan1000@gmail.com>
2023-04-07 21:46:27 +0530

    .

--- typeChecker.c
+++ typeChecker.c
@@ -27,6 +27,108 @@ MainTableEntry* createModule(char* name, ParamList* inputList, ParamList* output
 typechecker(ASTNode* astNode){
     int c = astNode->label;
     switch(c){
+        case PROGRAM:
+            for (ASTNode* current = astNode->child; current != NULL; current = current -> sibling)
+                typechecker(current);
+            break;
+
+        case MODULEDECLARATIONS:
+            for (ASTNode* curr = astNode->child; curr != NULL; curr = curr -> sibling)
+                typechecker(curr);
+            break;
+        
+        case OTHERMODULES1:
+            for (ASTNode* current = astNode->child; current != NULL; current = current -> sibling)
+                typechecker(current);
+            break;
+
+        case OTHERMODULES2:
+            for (ASTNode* current = astNode->child; current != NULL; current = current -> sibling)
+                typechecker(current);
+            break;
+
+        case MODULEDECLARATION:
+            if (searchModule(SymbolTable,astNode->tk->val.identifier))
+                printf("ERROR at line %d: Module \"%s\" has already been declared\n",astNode->tk->lineNo, astNode->tk->val.identifier);
+            else 
+                insertModule(SymbolTable,createModule(astNode->tk->val.identifier,NULL,NULL,NULL));
+            break;
+
+        case DRIVERMODULE:
+            curr = createModuleTable(20);
+            insertModule(SymbolTable,createModule("driver",NULL,NULL,curr));
+            typechecker(astNode->child);
+            break;
+
+        case MODULE:
+            MainTableEntry* searched = searchModule(SymbolTable, astNode->child->tk->val.identifier);
+            if (searched != NULL && searched->moduleTable != NULL)
+                printf("ERROR at line %d: Module \"%s\" has already been defined\n",astNode->tk->lineNo, astNode->tk->val.identifier);
+            else {
+                curr = createModuleTable(20);
+                if (searched == NULL)
+                    insertModule(SymbolTable,createModule(astNode->child->tk->val.identifier,NULL,NULL,curr));
+                else
+                    searched->moduleTable = curr;
+                ASTNode* current = astNode->child->sibling;
+                while (current != NULL) {
+                    if (current->label == INPUT_PLIST) {
+                        ASTNode* parameter = current -> child;
+                        ParamList* input_plist = NULL;
+                        while (parameter != NULL) {
+                            ParamList* newnode = (ParamList*)malloc(sizeof(ParamList));
+                            newnode->identifier = parameter->tk->val.identifier;
+                            typechecker(parameter->child);
+                            newnode->t = parameter->child->type;
+                            newnode->next = NULL;
+                            input_plist = insertlast(input_plist, newnode);
+                            parameter = parameter->sibling;
+                        }
+                        searched->inputList = input_plist;
+                    }
+                    else if (current->label = OUTPUT_PLIST) {
+                        ASTNode* parameter = current -> child;
+                        ParamList* output_plist = NULL;
+                        while (parameter != NULL) {
+                            ParamList* newnode = (ParamList*)malloc(sizeof(ParamList));
+                            newnode->identifier = parameter->tk->val.identifier;
+                            typechecker(parameter->child);
+                            newnode->t = parameter->child->type;
+                            newnode->next = NULL;
+                            output_plist = insertlast(input_plist, newnode);
+                            parameter = parameter->sibling;
+                        }
+                        searched->outputList = output_plist;
+                    }
+                    else {
+                        for (ASTNode* stmt = current->child; stmt != NULL; stmt = stmt->sibling)
+                            typechecker(stmt);
+                    }
+                    current = current -> sibling;
+                }
+            }
+            break;
+
+        case INTEGER_:
+            astNode->type.is_primitive = true;
+            astNode->type.pt = INTEGER;
+            break;
+
+        case REAL_:
+            astNode->type.is_primitive = true;
+            astNode->type.pt = REAL;
+            break;
+
+        case BOOLEAN_:
+            astNode->type.is_primitive = true;
+            astNode->type.pt = BOOLEAN;
+            break;
+
+        case ARRAY:
+            typechecker(astNode->child->sibling);
+            astNode->type.is_primitive = false;
+            astNode->type.pt = astNode->child->sibling->type;
+            astNode->type.lower_bound = 
 
         case 35: //case assign
             ASTNode* rhs = astNode->child->sibling;
