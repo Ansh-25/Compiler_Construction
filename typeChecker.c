f3e7e3031f94826da86202782afc912e6a03af25
Nikhil-Pradhan <nikhilpradhan1000@gmail.com>
2023-04-07 19:20:03 +0530

    typechecker functions

--- typeChecker.c
+++ typeChecker.c
@@ -1,52 +1,68 @@
-// #include "symbolTableDef.h"
+#include "symbolTableDef.h"
 
-// MainTableEntry** SymbolTable;
-// ModuleTableEntry** curr; //set curr at every module node
+MainTableEntry** SymbolTable;
+ModuleTableEntry** curr; //set curr at every module node
 
-// /* how to handle if module reuse before module definition */
+void createMainTable(int size) {
+    SymbolTable = (MainTableEntry**)malloc(size * sizeof(MainTableEntry*));
+    for (int i = 0; i < size; i ++)
+        SymbolTable[i] = NULL;
+}
 
-// typechecker(ASTNode* astNode){
-//     int c = astNode->label;
-//     switch(c){
+ModuleTableEntry** createModuleTable(int size) {
+    ModuleTableEntry** newTable = (ModuleTableEntry*)malloc(size * sizeof(ModuleTableEntry*));
+    for (int i = 0; i < size; i ++)
+        newTable[i] = NULL;
+}
 
-//         case 35: //case assign
-//             ASTNode* rhs = astNode->child->sibling;
-//             if(astNode->child->label==ARR_ASSIGN){ //case arr assign
-//                 ASTNode* lhs = astNode->child->child;
-//                 if(searchVar(curr,lhs->tk->val.identifier)==NULL){
-//                     //Error:variable not found
-//                 }
-//                 else if(searchVar(curr,lhs->tk->val.identifier)->t.t.at.pt!=rhs->type.t.pt){
-//                     //Type Error:datatypes don't match at line no. lhs->tk->lineno.
-//                 }
-//             }
-//             else{
-//                 ASTNode* lhs = astNode->child;
-//                 if(searchVar(curr,lhs->tk->val.identifier)==NULL){
-//                 //Error:variable not found
-//                 }
-//                 else if(compare_Datatype(searchVar(curr,lhs->tk->val.identifier)->t,rhs->type)!=true){
-//                     //Type Error:datatypes don't match at line no. lhs->tk->lineno.
-//                 }
-//             }          
-//             typechecker(astNode->sibling);
-//             break;
+MainTableEntry* createModule(char* name, ParamList* inputList, ParamList* outputList, ModuleTableEntry** table) {
+    MainTableEntry* newModule = malloc(sizeof(MainTableEntry));
+    newModule->module_name = name;
+    newModule->inputList = inputList;
+    newModule->outputList = outputList;
+    newModule->moduleTable = table;
+    return newModule;
+}
+
+typechecker(ASTNode* astNode){
+    int c = astNode->label;
+    switch(c){
+
+        case 35: //case assign
+            ASTNode* rhs = astNode->child->sibling;
+            if(astNode->child->label==ARR_ASSIGN){ //case arr assign
+                ASTNode* lhs = astNode->child->child;
+                if(searchVar(curr,lhs->tk->val.identifier)==NULL){
+                    //Error:variable not found
+                }
+                else if(searchVar(curr,lhs->tk->val.identifier)->t.t.at.pt!=rhs->type.t.pt){
+                    //Type Error:datatypes don't match at line no. lhs->tk->lineno.
+                }
+            }
+            else{
+                ASTNode* lhs = astNode->child;
+                if(searchVar(curr,lhs->tk->val.identifier)==NULL){
+                //Error:variable not found
+                }
+                else if(compare_Datatype(searchVar(curr,lhs->tk->val.identifier)->t,rhs->type)!=true){
+                    //Type Error:datatypes don't match at line no. lhs->tk->lineno.
+                }
+            }          
+            typechecker(astNode->sibling);
+            break;
 
 
-//         case 38: //case declare
-//             DataType d = astNode->child->sibling->type;
-//             ASTNode* idList = astNode->child->child;
-//             while(idList!=NULL){
-//                 ModuleTableEntry* new_entry = (ModuleTableEntry*)malloc(sizeof(ModuleTableEntry));
-//                 new_entry->identifier = idList->tk->val.identifier;
-//                 new_entry->t = d;
-//                 insertVar(curr,new_entry);
-//                 idList = idList->sibling;
-//             }
-//             typechecker(astNode->sibling);
-//             break;
-
-        
-
-//     }
-// }
+        case 38: //case declare
+            DataType d = astNode->child->sibling->type;
+            ASTNode* idList = astNode->child->child;
+            while(idList!=NULL){
+                ModuleTableEntry* new_entry = (ModuleTableEntry*)malloc(sizeof(ModuleTableEntry));
+                new_entry->identifier = idList->tk->val.identifier;
+                new_entry->t = d;
+                insertVar(curr,new_entry);
+                idList = idList->sibling;
+            }
+            typechecker(astNode->sibling);
+            break;
+    }
+}
